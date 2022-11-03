a7a4ddfd9c002d18bb389a0e2d39347c0b085de0
tatarkin <mobtaxi@gmail.com>
2022-11-03 20:16:28 +0300

    add cat

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import UserInfo from "./components/userInfo";
 
 function App() {
   const workSkill = ["shop","fabric","school"];
+  const cat={nickName:"Vasya", age:5};
   const homePets =  ["cat","dog","mouse","snake","bull"];
+  console.log(cat);
+
   const user = {
     firstName: "Vanya",
     age: 39,
