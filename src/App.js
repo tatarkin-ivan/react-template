1076743cd667247284a15ed932917cbe82a869f8
tatarkin <mobtaxi@gmail.com>
2022-11-03 20:12:45 +0300

    change data2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import UserInfo from "./components/userInfo";
 
 function App() {
   const workSkill = ["shop","fabric","school"];
+  const dog={nickname: 'Bobik', age: 11};
   const homePets =  ["cat","dog","mouse","snake","bull","cowgit "];
+  console.log(dog);
   const user = {
     firstName: "Vanya",
     age: 39,
